1a4457f53e6d99322b7ea252369683edb587f9fd
VitorNasc8 <vitornasc8k13@gmail.com>
2023-10-14 11:45:50 -0300

    Criação da paginas de avaliar laudo

---
+++ workspace/LaudosDeCardiologia/src/main/java/vsvn/tsi/daw/cardio/logica/ObterLaudosParaAvaliacao.java
@@ -0,0 +1,18 @@
+package vsvn.tsi.daw.cardio.logica;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ObterLaudosParaAvaliacao implements Logica{
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
